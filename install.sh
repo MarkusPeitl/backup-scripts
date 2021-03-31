6c6118556e29d8e266d990938732ae950c320059
Markus Peitl <office@markuspeitl.com>
2021-03-31 20:50:10 +0200

    Added auto detect install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+systemhasprogram() {
+    [ -x "$(which $1)" ]
+}
+
+detectinstallpkg() {
+	if systemhasprogram apt-get ; then
+		sudo apt install $1
+	elif systemhasprogram pacman ; then
+		sudo pacman -S $1
+	elif systemhasprogram yum ; then
+		sudo yum install $1
+	else
+		echo 'No package manager found - not installing $1'
+	fi;
+}
+
+detectinstallpkg rsync
+
+#detectinstallpkg rdiff-backup
+#detectinstallpkg timeshift
+#detectinstallpkg bup
